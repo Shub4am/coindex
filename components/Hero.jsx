9c1db79804e4b3178ea1e7230399e5e570a42e44
Shub4am <shub4am@gmail.com>
2025-01-07 00:33:15 +0530

    Update btn hover

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,25 +1,24 @@
-import Image from 'next/image';
-import React from 'react';
-import HeroBg from '../assets/images/Ellipse1.png';
-import Success from '@/pages/success';
-import { useRouter } from 'next/router';
+import Image from "next/image";
+import React from "react";
+import HeroBg from "../assets/images/Ellipse1.png";
+import Success from "@/pages/success";
+import { useRouter } from "next/router";
 
 const Hero = () => {
   const router = useRouter();
   const handleJoinUs = () => {
-    console.log('yeah');
-    router.push('/success');
+    router.push("/success");
   };
   return (
     <div className="mt-[112px]">
       <div>
         <h1 className="font-semibold text-5xl sm:text-[80px] leading-[110px]  text-center .tracking-tighter capitalize">
           the world&apos;s fastest growing
-          <br />{' '}
+          <br />{" "}
           <span className="uppercase bg-gradient-to-r from-[#1d93d8] to-[#ff00cc] text-transparent bg-clip-text">
-            {' '}
+            {" "}
             crypto
-          </span>{' '}
+          </span>{" "}
           community
         </h1>
         <h2 className="font-normal text-base px-10 sm:px-0 sm:text-2xl leading-[34px] text-center text-[#FFFFFFCC] mt-[30px]">
@@ -39,7 +38,7 @@ const Hero = () => {
           />
           <button
             onClick={handleJoinUs}
-            className="hidden sm:absolute inset-y-0 right-0 sm:flex justify-center items-center p-5 gap-[10px] bg-pinkBtn text-white rounded-full capitalize mb-1 mt-1 mr-1"
+            className="hidden sm:absolute inset-y-0 right-0 sm:flex justify-center items-center p-5 gap-[10px] bg-pinkBtn hover:bg-[#b6276e] text-white rounded-full capitalize mb-1 mt-1 mr-1"
           >
             join us
           </button>
